3729de0b946f92e0121fe2ff4ca92f22130de9a8
Ivan Gochev <redial@abv.bg>
2020-05-07 12:38:25 +0300

    refactored gateways

--- movies/movies_getaway.py
+++ movies/movies_getaway.py
@@ -7,54 +7,40 @@ from movies.models import MovieModel
 class MovieGateway:
     def __init__(self):
         self.model = MovieModel
-
+        self.db = Database()
     def create(self, *, name, rating):
-        db = Database()
-        self.model.validate(name, rating)
-        db.cursor.execute(CREATE_MOVIE, (name, rating))
-        db.connection.commit()
-        db.connection.close()
+        with self.db.connection:
+            self.model.validate(name, rating)
+            self.db.cursor.execute(CREATE_MOVIE, (name, rating))
 
     def get_by_id(self, *, movie_id):
-        db = Database()
-        db.cursor.execute(GET_BY_ID, (movie_id))
+        with self.db.connection:
+            self.db.cursor.execute(GET_BY_ID, (movie_id))
 
-        movie_db = db.cursor.fetchall()
-        movie = self.model.convert(movie_db[0])
-        db.connection.commit()
-        db.connection.close()
-
+            movie_db = self.db.cursor.fetchall()
+            movie = self.model.convert(movie_db[0])
         return movie
 
     def get_all_movies_ordered_by_rating(self):
-        db = Database()
-        db.cursor.execute(GET_ALL_MOVIES_ORDERED_BY_RATING)
+        with self.db.connection:
+            self.db.cursor.execute(GET_ALL_MOVIES_ORDERED_BY_RATING)
 
-        movies_db = db.cursor.fetchall()
-        result = []
-        for db_movie in movies_db:
-            movie = self.model.convert(db_movie)
-            result.append(movie)
-
-        db.connection.commit()
-        db.connection.close()
+            movies_db = self.db.cursor.fetchall()
+            result = []
+            for db_movie in movies_db:
+                movie = self.model.convert(db_movie)
+                result.append(movie)
 
         return result
 
     def update_movie_name(self, movie_id, new_name):
-        db = Database()
-        db.cursor.execute(UPDATE_MOVIE_NAME, (new_name, movie_id))
-        db.connection.commit()
-        db.connection.close()
+        with self.db.connection:
+            self.db.cursor.execute(UPDATE_MOVIE_NAME, (new_name, movie_id))
 
     def update_movie_rating(self, movie_id, new_rating):
-        db = Database()
-        db.cursor.execute(UPDATE_MOVIE_RATING, (new_rating, movie_id))
-        db.connection.commit()
-        db.connection.close()
+        with self.db.connection:
+            self.db.cursor.execute(UPDATE_MOVIE_RATING, (new_rating, movie_id))
 
     def delete_movie(self, movie_id):
-        db = Database()
-        db.cursor.execute(DELETE_MOVIE, (movie_id))
-        db.connection.commit()
-        db.connection.close()
+        with self.db.connection:
+            self.db.cursor.execute(DELETE_MOVIE, (movie_id))
